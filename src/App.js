6f1b0fc4ffd6158411850540b77b9a8c62c3fe5e
Mohammad Mahdi Niazi <m.mahdi.niazi@gmail.com>
2023-08-17 20:21:12 +0430

    Add Initial setup for bookstore App Add Routes Add components

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Books from "./pages/Books";
+import Navigation from "./components/Navigation";
+import Categories from "./pages/Catagories";
+
+// import Navigation from "./components/Navigation";
+
+function App() {
+  return (
+    <>
+      <header>
+        <Navigation />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/bookstore-cms/categories" element={<Categories />} />
+        </Routes>
+      </main>
+    </>
+  );
+}
+
+export default App;
